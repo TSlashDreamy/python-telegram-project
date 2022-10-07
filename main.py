6f427b14811e0625f69a9a050668d011de13a82f
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2022-10-07 20:46:44 +0300

    added buttons
    basic functionality - shitty code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
+from telebot import types
 
-API_TOKEN = 'Nope'
+API_TOKEN = 'nope'
 bot = telebot.TeleBot(API_TOKEN)
 me = bot.get_me()
 
@@ -10,8 +11,28 @@ print(f'logged in: {me.first_name}')
 # handler - listening to user commands
 @bot.message_handler(commands=['start'])
 def start(message):
-    content = f'Hi, {message.from_user.username} i`m working!'
-    bot.send_message(message.chat.id, content, parse_mode='html')
+    content = f'Hi, <b>{message.from_user.username}</b> i`m working!\n<u>(Buttons not working now)</u>'
+    out_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    commands = types.KeyboardButton('📚 Commands')
+    website_button = types.KeyboardButton('/website')
+    out_markup.add(commands, website_button)
+
+    bot.send_message(message.chat.id, content, parse_mode='html', reply_markup=out_markup)
+
+
+@bot.message_handler(commands=['website'])
+def website(message):
+    in_markup = types.InlineKeyboardMarkup()
+    website_link = types.InlineKeyboardButton('Веб-сайт ІДТКД', url='https://idtd.nltu.edu.ua/')
+    in_markup.add(website_link)
+
+    bot.send_message(message.chat.id, "Website link", parse_mode='html', reply_markup=in_markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_commands(message):
+    if message.text == "📚 Commands":
+        bot.send_message(message.chat.id, "Whoops, working on it", parse_mode='html')
 
 
 bot.infinity_polling()
