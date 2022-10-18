eca516def138469c1bf35433e86b22aada77c2b7
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2022-10-18 21:48:25 +0300

    core partial refactoring

---
+++ core/daySwitcher.py
@@ -0,0 +1,16 @@
+def switch_day(inner):
+    match inner:
+        case 0:
+            return "\nMonday\n================================="
+
+        case 1:
+            return "\nTuesday\n================================="
+
+        case 2:
+            return "\nWednesday\n================================="
+
+        case 3:
+            return "\nThursday\n================================="
+
+        case 4:
+            return "\nFriday\n================================="
