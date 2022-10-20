51ab885b19c0a85d99c95ededab5652021434d47
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2022-10-20 21:30:46 +0300

    updated command list

--- commands/commandsList.py
+++ commands/commandsList.py
@@ -9,7 +9,8 @@ def show_commands(message):
     out_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     schedule_button = types.KeyboardButton('📃 Розклад')
     date_button = types.KeyboardButton('📅 Точна дата')
-    out_markup.add(schedule_button, date_button)
+    diary_button = types.KeyboardButton('📒 Записник')
+    out_markup.add(schedule_button, date_button, diary_button)
     bot.send_message(message.chat.id, "Оберіть бажану команду однією з кнопок нижче",
                      parse_mode='html', reply_markup=out_markup)
     bot.delete_message(message.chat.id, message.message_id)
