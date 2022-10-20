d1ded01bbb7b75d64017703a8ac700829376104f
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2022-10-20 21:33:03 +0300

    added notes manipulations

---
+++ diary_notes/getDiaryData.py
@@ -0,0 +1,33 @@
+#-*- coding: cp1251 -*-
+import sqlite3
+
+
+# cur.executescript("""
+# CREATE TABLE if not exists DZ (
+#    Num INT  PRIMARY KEY NOT NULL,
+#    UserId int NOT NULL,
+#    name char NOT NULL,
+#    data DATE NOT NULL
+# );""")
+
+
+def get_data():
+    try:
+        con = sqlite3.connect("diary_notes/DZ_db.db", check_same_thread=False)
+        cur = con.cursor()
+        cur.execute('select * From DZ')
+        data = cur.fetchall()
+        con.close()
+        print(data)
+        if not data:
+            print("No data")
+            return 0
+        return data
+    except:
+        print("No data")
+        return 0
+
+
+# con.commit()
+# print(cur.fetchall())
+
