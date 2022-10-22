8b6b6ebb2f7f3a1c4bf24e6077fbecb5c94b1746
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2022-10-22 22:07:13 +0300

    bug fix

--- commands/diaryRemember.py
+++ commands/diaryRemember.py
@@ -12,17 +12,25 @@ types = botCore.types
 
 def remember_data(message):
     global number
+    count = 0
     current_date = dateCalculation.today.strftime("%d/%m/%Y")
     data = getDiaryData.get_data()
-    if data != 0:
-        for i in range(len(data)):
-            i+=1
-            number = i
-    else:
-        number = 0
+    if diaryTrigger.input_for_user == message.from_user.id:
+        if data != 0:
+            number = 0
+            for i in range(len(data)):
+                if data[i][1] == message.from_user.id:
+                    count += 1
+                    i += 1
+                    number = i
 
-    setDiaryData.database_input(number + 1, message.text, current_date, message.from_user.id)
+        else:
+            number = 0
+    else:
+        return
+    setDiaryData.database_input(count + 1, message.text, current_date, message.from_user.id)
     bot.send_message(message.chat.id, "✅ Запис внесено",
                      parse_mode='html')
+    diaryTrigger.input_for_user = 0
     diaryTrigger.inputting_note = False
     diaryList.show_commands(message)
--- commands/diaryTrigger.py
+++ commands/diaryTrigger.py
@@ -2,8 +2,9 @@ import time
 from telebot.types import ReplyKeyboardRemove
 from core import botCore
 
-global inputting_note
+global inputting_note, input_for_user
 inputting_note = False
+input_for_user = 0
 
 
 types = botCore.types
@@ -12,9 +13,17 @@ me = botCore.me
 
 
 def trigger_event(message):
-    global inputting_note
-    bot.send_message(message.chat.id, "✒ Введіть Ваш запис: ",
-                     parse_mode='html', reply_markup=ReplyKeyboardRemove())
-    bot.delete_message(message.chat.id, message.message_id)
-    time.sleep(0.5)
-    inputting_note = True
+    global inputting_note, input_for_user
+    if input_for_user == 0:
+        input_for_user = message.from_user.id
+        bot.send_message(message.chat.id, "✒ Введіть Ваш запис: ",
+                         parse_mode='html', reply_markup=ReplyKeyboardRemove())
+        bot.delete_message(message.chat.id, message.message_id)
+        # safe sleep
+        time.sleep(0.2)
+        inputting_note = True
+    else:
+        bot.send_message(message.chat.id, "⚠️О ні!\nНа даний момент це не доступно :с\nПовторіть спробу через декілька "
+                                          "секунд ",
+                         parse_mode='html')
+        bot.delete_message(message.chat.id, message.message_id)
